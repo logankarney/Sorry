1bbc10c304dbd604a07ee70608908a030627149b
emerson <git@emersonveenstra.net>
2018-11-27 12:31:58 -0500

    Change default port to 12000 because it's our favorite number

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ from src.SorryServer import SorryServer
 
 parser = argparse.ArgumentParser(description="Start the Sorry! server")
 parser.add_argument("--host", default="127.0.0.1", type=str)
-parser.add_argument("--port", default=50444, type=int)
+parser.add_argument("--port", default=12000, type=int)
 
 args = parser.parse_args()
 
