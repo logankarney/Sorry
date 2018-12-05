77548fa1827e626729cbda073b36f307fc8c259e
Logan Karney <karneyl@mail.gvsu.edu>
2018-12-04 20:55:36 -0500

    Restructured how the tiles are stored, tiles are now able to enter the home spot, if they are not currently on the previous row

--- src/sample/Moves.java
+++ src/sample/Moves.java
@@ -10,14 +10,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Moves {
-    /** The outside rows for each color */
+    /** The  rows for each color */
     private static TileButton[] redRow, blueRow, greenRow, yellowRow;
-
-    /** the inside lane for each color */
-    private static TileButton[] redHome, blueHome, greenHome, yellowHome;
-
-    /** The spawn for each color, red's is 0, blue's is 1, ect */
-    private static TileButton[] spawns;
 
     private static ArrayList<TileButton> moves;
 
@@ -32,34 +26,23 @@ public class Moves {
         this.blueRow = controller.getBlueRow();
         this.yellowRow = controller.getYellowRow();
         this.greenRow = controller.getGreenRow();
-        this.redHome = controller.getRedHome();
-        this.blueHome = controller.getBlueHome();
-        this.yellowHome = controller.getYellowHome();
-        this.greenHome = controller.getGreenHome();
-        this.spawns = controller.getSpawns();
-
-
-
-        for(int i = 0; i < 4; i++){
-            spawns[i].setOccupiedBy(4);
-        }
 
         //Have to explicitly set this as lamda's do not like assigning on actions in loops
-        spawns[0].setOnAction(e -> {reset(); displayMoves(spawns[0], spawns[0].getC(), controller.cardValue);});
-        spawns[0].setPieceColor(TileColor.RED);
-        spawns[0].setPicture(controller.redPiece);
+        redRow[22].setOnAction(e -> {reset(); displayMoves(redRow[22], redRow[22].getC(), controller.cardValue);});
+        redRow[22].setPieceColor(TileColor.RED);
+        redRow[22].setPicture(controller.redPiece);
 
-        spawns[1].setOnAction(e -> { reset(); displayMoves(spawns[1], spawns[1].getC(), controller.cardValue);});
-        spawns[1].setPieceColor(TileColor.BLUE);
-        spawns[1].setPicture(controller.bluePiece);
+        blueRow[22].setOnAction(e -> { reset(); displayMoves(blueRow[22], blueRow[22].getC(), controller.cardValue);});
+        blueRow[22].setPieceColor(TileColor.BLUE);
+        blueRow[22].setPicture(controller.bluePiece);
 
-        spawns[2].setOnAction(e -> { reset(); displayMoves(spawns[2], spawns[2].getC(), controller.cardValue);});
-        spawns[2].setPieceColor(TileColor.YELLOW);
-        spawns[2].setPicture(controller.yellowPiece);
+        yellowRow[22].setOnAction(e -> { reset(); displayMoves(yellowRow[22], yellowRow[22].getC(), controller.cardValue);});
+        yellowRow[22].setPieceColor(TileColor.YELLOW);
+        yellowRow[22].setPicture(controller.yellowPiece);
 
-        spawns[3].setOnAction(e -> {reset(); displayMoves(spawns[3], spawns[3].getC(), controller.cardValue);});
-        spawns[3].setPieceColor(TileColor.GREEN);
-        spawns[3].setPicture(controller.greenPiece);
+       greenRow[22].setOnAction(e -> {reset(); displayMoves(greenRow[22], greenRow[22].getC(), controller.cardValue);});
+        greenRow[22].setPieceColor(TileColor.GREEN);
+        greenRow[22].setPicture(controller.greenPiece);
 
         moves = new ArrayList<TileButton>();
 
@@ -78,12 +61,12 @@ public class Moves {
 
         pieces.add(greenRow[10]);
 */
-        redHome[0].setPicture(controller.redPiece);
-        redHome[0].setPieceColor(TileColor.RED);
-        redHome[0].setOccupiedBy(1);
-        redHome[0].setOnAction(e -> {
-            displayMoves(redHome[0], TileColor.RED, controller.cardValue);
-        });
+        //redHome[0].setPicture(controller.redPiece);
+        //redHome[0].setPieceColor(TileColor.RED);
+        //redHome[0].setOccupiedBy(1);
+        //redHome[0].setOnAction(e -> {
+           // displayMoves(redHome[0], TileColor.RED, controller.cardValue);
+        //});
     }
 
     public void movePiece(TileButton oldPiece, TileButton newPiece, TileColor playerColor, boolean swap){
@@ -91,7 +74,7 @@ public class Moves {
         TileButton spawn = null;
 
         if(newPiece.getPicture() != null && swap == false) {
-            spawn= getColorSpawn(newPiece.getPieceColor());
+            spawn = getColorRow(newPiece.getPieceColor())[22];
             spawn.setOccupiedBy(spawn.getOccupiedBy()+1);
         }
 
@@ -169,29 +152,28 @@ public class Moves {
             TileButton[] currentRow = getColorRow(origSpot.getC());
 
             //condition for entering the home row
-            if(spot < 2 && origSpot.getC() == playerColor){
+            if(spot < 2 && origSpot.getC() == playerColor ){
 
-                if(moveAmount < 2)
-                    moves.add(currentRow[moveAmount + 1]);
+                if(moveAmount + origSpot.getSpot() + 15 < 22)
+                    moves.add(currentRow[moveAmount + 15]);
 
-               if(spot + 6 > moveAmount) {
-                   TileButton[] row = getColorHome(playerColor);
-                   moves.add(row[moveAmount - 1]);
-               }
+               //if(spot + 6 > moveAmount) {
+                   //TileButton[] row = getColorHome(playerColor);
+                   //moves.add(row[moveAmount - 1]);
+               //}
                return moves;
             }
 
-            //21 is the 5th element
             //for when the piece is inside its homerow
             if(spot > 15){
-                TileButton[] row = getColorHome(origSpot.getC());
-                if(moveAmount + spot < 21){
-                    System.out.println(spot);
-                    moves.add(row[21 - spot + moveAmount]);
+                //TileButton[] row = getColorHome(origSpot.getC());
+                if(moveAmount + spot < 22){
+                    //System.out.println(spot);
+                  moves.add(currentRow[spot + moveAmount]);
                     return moves;
                 }
-                else if(spot != 22)
-                    moves.add(row[5]);
+              //  else if(spot != 22)
+                  //  moves.add(row[5]);
             }
 
             //if the card is 4
@@ -292,45 +274,6 @@ public class Moves {
         return row;
     }
 
-    public TileButton[] getColorHome(TileColor c){
-        TileButton[] row = redRow;
-        switch (c){
-            case RED:
-                row = redHome;
-                break;
-            case BLUE:
-                row = blueHome;
-                break;
-            case YELLOW:
-                row = yellowHome;
-                break;
-            case GREEN:
-                row = greenHome;
-                break;
-        }
-
-        return row;
-    }
-
-    public TileButton getColorSpawn(TileColor c){
-        TileButton spawn = spawns[0];
-        switch (c){
-            case RED:
-                spawn = spawns[0];
-                break;
-            case BLUE:
-                spawn = spawns[1];
-                break;
-            case YELLOW:
-                spawn = spawns[2];
-                break;
-            case GREEN:
-                spawn = spawns[3];
-                break;
-        }
-
-        return spawn;
-    }
 
     public  void removeCss(ArrayList<TileButton> moves){
         for(TileButton t : moves){
@@ -388,7 +331,7 @@ public class Moves {
     public ArrayList<TileButton> getEnemyPieces(TileColor playerColor){
         ArrayList<TileButton> moves= new ArrayList<TileButton>();
 
-        for(int i = 0; i < redRow.length; i++){
+        for(int i = 0; i < 16; i++){
             if(redRow[i].getPieceColor() != playerColor && redRow[i].getPieceColor() != null)
                 moves.add(redRow[i]);
             if(blueRow[i].getPieceColor() != playerColor && blueRow[i].getPieceColor() != null)
